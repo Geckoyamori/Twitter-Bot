010517ecddf64c7ad77e5c1e3bd033bb1a3d239a
Geckoyamori <69742111+Geckoyamori@users.noreply.github.com>
2023-08-15 13:28:50 +0900

    update

--- ArticleSummary/script.js
+++ ArticleSummary/script.js
@@ -1,30 +1,29 @@
-// フォームの参照を取得
-const form = document.getElementById("url-form");
-// 結果を表示するエリアの参照を取得
-const resultDiv = document.getElementById("result");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("extractor-form");
+  const resultDiv = document.getElementById("result");
 
-// フォームが送信されたときのイベントリスナーを設定
-form.addEventListener("submit", function (event) {
-  // フォームのデフォルトの送信動作を防ぐ
-  event.preventDefault();
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-  // 入力されたURLを取得
-  const url = document.getElementById("url-input").value;
+    const url = e.target.url.value;
+    const apiURL = `https://twitter-application.onrender.com/extract_content/?url=${encodeURIComponent(
+      url
+    )}`;
 
-  // APIのURLを構築
-  const apiURL = `https://twitter-application.onrender.com/extract_content/?url=${encodeURIComponent(
-    url
-  )}`;
+    resultDiv.textContent = "Loading...";
 
-  // APIにリクエストを送る
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((data) => {
-      // 結果を表示エリアに設定
-      resultDiv.textContent = JSON.stringify(data, null, 2);
-    })
-    .catch((error) => {
-      // エラーが発生した場合、それを表示エリアに表示
-      resultDiv.textContent = "エラーが発生しました: " + error;
-    });
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+
+      if (data.content) {
+        resultDiv.textContent = data.content;
+      } else {
+        resultDiv.textContent = "No content found";
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      resultDiv.textContent = "An error occurred while fetching data";
+    }
+  });
 });
